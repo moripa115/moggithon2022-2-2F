5e437a4db2591807e584845a3b42845e00ce798a
Yuta0227 <yutahonjo@keio.jp>
2022-02-10 12:44:49 +0900

    制作物完成

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const time = document.getElementById('time');
-function show2digits(num){
+const greeting =document.getElementById('greeting');
+const username=document.getElementById('name');
+const focus=document.getElementById('focus');
+function show2Digits(num){
     //常に二桁表示
     let tmp;
     if (num<10){
@@ -9,11 +12,32 @@ function show2digits(num){
     }
     return tmp;
 }
+//名前入力
+username.innerHTML="[ENTER NAME]";
+//改行無効
+username.onkeydown=function(e){
+    if(e.key=="Enter"){
+        return false;
+    }
+};
+focus.innerHTML="[ENTER FOCUS]";
 setInterval(function(){
-    //一秒ごとに時分秒表示
-    let currentTime = new Date();
-    let currentHour = show2digits(currentTime.getHours());
-    let currentMinute = show2digits(currentTime.getMinutes());
-    let currentSecond = show2digits(currentTime.getSeconds());
+    //0秒ごとに時分秒表示==常に更新
+    const currentTime = new Date();
+    const currentHour = show2Digits(currentTime.getHours());
+    const currentMinute = show2Digits(currentTime.getMinutes());
+    const currentSecond = show2Digits(currentTime.getSeconds());
     time.innerHTML=`${currentHour}:${currentMinute}:${currentSecond}`;
-},1000)
+    function greetingFunc(){
+        if(currentHour>=4&&currentHour<10){
+            greeting.innerHTML=`おはようございます。`;
+        }
+        if(currentHour>=10&&currentHour<19){
+            greeting.innerHTML=`こんにちは。`;
+        }
+        if((currentHour>=19&&currentHour<24)||(currentHour>=0&&currentHour<4)){
+            greeting.innerHTML=`こんばんは。`;
+        }
+    };
+    greetingFunc();
+},0);
